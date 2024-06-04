96317089b139841bbfcb0927d50a71b2954230fe
eshita241 <eshitachokhani2003@gmail.com>
2024-06-04 14:56:07 +0530

    something added to the basic setup

--- components/dashboard/Navbar.jsx
+++ components/dashboard/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { TbHistoryToggle } from "react-icons/tb";
+
 
 export default function Navbar() {
   return (
-    <div className=' bg-slate-300'>
-        <h2>The Navbar is here </h2>
+    <div className=' bg-slate-300 h-14 flex items-center justify-between px-7'>
+        <div className='flex'>
+          <button><TbHistoryToggle size={34} className=''/></button>
+        </div>
+        <div></div>
     </div>
   )
 }
