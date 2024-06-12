632837278749cb559335e186de2d8c0018b680e6
eshita241 <eshitachokhani2003@gmail.com>
2024-06-12 20:07:17 +0530

    idr what changes

--- components/dashboard/Navbar.jsx
+++ components/dashboard/Navbar.jsx
@@ -1,14 +1,47 @@
 import React from 'react';
 import { TbHistoryToggle } from "react-icons/tb";
+import { IoSearch } from "react-icons/io5";
+import { FaPlus } from "react-icons/fa6";
 
 
 export default function Navbar() {
   return (
     <div className=' bg-slate-300 h-14 flex items-center justify-between px-7'>
-        <div className='flex'>
+        <div className='flex gap-5'>
           <button><TbHistoryToggle size={34} className=''/></button>
+          
+<form class=" max-w-lg mx-auto">
+    <div class="flex">
+        {/*<label for="search-dropdown" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your Email</label>
+        <button id="dropdown-button" data-dropdown-toggle="dropdown" class="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" type="button">All categories <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+  </svg></button>*/}
+        <div class="relative w-full">
+            <input type="search" id="search-dropdown" class="block p-2.5 w-full rounded-lg z-20 text-sm hover:w-96 text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search" required />
+            <button type="submit" class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              {/*<svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+  </svg>*/}
+  <IoSearch size={20}/>
+
+                <span class="sr-only">Search</span>
+            </button>
         </div>
-        <div></div>
+    </div>
+</form>
+
+        </div>
+        <div>
+        
+        <button data-tooltip-target="tooltip-dark" type="button" class="ms-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><FaPlus size={20} classNameName="cursor-pointer" /></button>
+
+<div id="tooltip-dark" role="tooltip" className="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
+    Tooltip content
+    <div className="tooltip-arrow" data-popper-arrow></div>
+</div>
+        
+
+        </div>
     </div>
   )
 }
---
+++ components/dashboard/searchInput.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function searchInput() {
+  return (
+    <div>searchInput</div>
+  )
+}
