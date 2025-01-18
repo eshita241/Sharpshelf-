fec8f52a89f2197b80a35b06c3553bb030d105d4
eshita241 <eshitachokhani2003@gmail.com>
2025-01-18 13:01:40 +0530

    added minor changes

--- app/page.js
+++ app/page.js
@@ -8,10 +8,8 @@ export default function Home() {
     <div className="flex items-center justify-center min-h-screen flex-col">
       <h2 className="text-3xl mb-8">Factory Management System</h2>
       <div className="flex gap-10">
-      <Link href="/dashboard/inventory"><div className="h-40 w-60 flex justify-center items-center border-4 border-neutral-950 rounded-3xl p-10 text-xl font-semibold hover:bg-slate-600 hover:bg-opacity-20">Inventory</div></Link>
-      <Link href="/dashboard/attendance"><div className="h-40 w-60 flex justify-center items-center border-4 border-neutral-950 rounded-3xl p-10 text-xl font-semibold hover:bg-slate-600 hover:bg-opacity-20">Attendance</div></Link>
-      <Link href="/dashboard/production"><div className="h-40 w-60 flex justify-center items-center border-4 border-neutral-950 rounded-3xl p-10 text-xl font-semibold hover:bg-slate-600 hover:bg-opacity-20">Production</div></Link>
-
+      <Link href="/dashboard/inventory"><div className="h-40 w-60 flex justify-center items-center border-4 border-neutral-950 rounded-3xl p-10 text-xl font-semibold hover:bg-slate-600 hover:bg-opacity-20">Red Cow</div></Link>
+      <Link href="/dashboard/inventory"><div className="h-40 w-60 flex justify-center items-center border-4 border-neutral-950 rounded-3xl p-10 text-xl font-semibold hover:bg-slate-600 hover:bg-opacity-20">Modern</div></Link>
       </div>
     </div> 
   );
